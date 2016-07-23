8c7c1c35b601f3bde2ad7e9d49736370015a7a7d
Rachel Prehodka-Spindel <RachelPS1125@gmail.com>
2016-07-23 11:05:42 -0400

    Working FizzBuzz

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	var countTo = prompt('Please enter the number you\'d like to count to.');
+	countTo = parseInt(countTo);
+
+	for (var counter = 1; counter <= countTo; counter ++){
+		if ((counter%5 === 0)&(counter%3 === 0)){
+			$('body').append('<p>"FizzBuzz"</p>');
+		}
+		else if (counter%5 === 0){
+			$('body').append('<p>"Buzz"</p>');
+		}
+		else if (counter%3 === 0){
+			$('body').append('<p>"Fizz"</p>');
+		}else{
+			$('body').append('<p>' +counter +'</p>');
+		};
+	};
+})
