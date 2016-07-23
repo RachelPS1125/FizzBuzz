c72693d79ccc29f926b40d4b02485f7eea31afc8
Rachel Prehodka-Spindel <RachelPS1125@gmail.com>
2016-07-23 13:03:17 -0400

    created fizzBuzz function

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 $(document).ready(function(){
 	var countTo = prompt('Please enter the number you\'d like to count to.');
-	while (isNaN(countTo)){
-		alert('please enter a valid number');
+	while (isNaN(countTo) || (countTo%1 != 0)){
+		alert('please enter a whole number');
 		countTo = prompt('Please enter the number you\'d like to count to.');
 	}
-	for (var counter = 1; counter <= countTo; counter ++){
-		if ((counter%5 === 0)&(counter%3 === 0)){
-			$('body').append('<p>"FizzBuzz"</p>');
-		}
-		else if (counter%5 === 0){
-			$('body').append('<p>"Buzz"</p>');
-		}
-		else if (counter%3 === 0){
-			$('body').append('<p>"Fizz"</p>');
-		}else{
-			$('body').append('<p>' +counter +'</p>');
+	countTo = parseInt(countTo);
+	function fizzBuzz(endNumber){
+		for (var counter = 1; counter <= endNumber; counter ++){
+			if ((counter%5 === 0)&(counter%3 === 0)){
+				$('body').append('<p>"FizzBuzz"</p>');
+			}
+			else if (counter%5 === 0){
+				$('body').append('<p>"Buzz"</p>');
+			}
+			else if (counter%3 === 0){
+				$('body').append('<p>"Fizz"</p>');
+			}else{
+				$('body').append('<p>' +counter +'</p>');
+			};
 		};
 	};
+	fizzBuzz(countTo);
 });
